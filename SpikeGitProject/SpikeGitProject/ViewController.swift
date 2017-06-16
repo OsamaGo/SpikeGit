9a0ee815c9a812869ebad61ec55c55d480d7482a
Osama Jassim <osama.jassim.go@gmail.com>
2017-06-16 07:00:06 +0300

    First Commit

---
+++ SpikeGitProject/SpikeGitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpikeGitProject
+//
+//  Created by Osama Jassim on 6/16/17.
+//  Copyright © 2017 Osama Jassim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
